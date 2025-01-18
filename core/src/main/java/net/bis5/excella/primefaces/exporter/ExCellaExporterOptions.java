becfdab7f1659d377bd50a0a321c4400cc7047ce
Takayuki Maruyama <bis5.wsys@gmail.com>
2025-01-19 07:11:06 +0900

    3.x: feat: Add BeforeWriteResponseListener for pre-export processing
    #89
    
    BREAKING CHANGE: some methods are added to ExCellaExporterOptions

--- core/src/main/java/net/bis5/excella/primefaces/exporter/ExCellaExporterOptions.java
+++ core/src/main/java/net/bis5/excella/primefaces/exporter/ExCellaExporterOptions.java
@@ -1,5 +1,9 @@
 package net.bis5.excella.primefaces.exporter;
 
+import java.io.IOException;
+
+import net.bis5.excella.primefaces.exporter.listener.BeforeWriteResponseListener;
+
 /**
  * ExCella exporter options
  */
@@ -11,4 +15,30 @@ public interface ExCellaExporterOptions {
      */
     boolean isThrowExceptionWhenNoData();
 
+    /**
+     * Add {@link BeforeWriteResponseListener} to execute before writing response.
+     * @param listener the listener to add
+     */
+    void addBeforeWriteResponseListener(BeforeWriteResponseListener listener);
+
+    /**
+     * Remove {@link BeforeWriteResponseListener}.
+     * @param listener the listener to remove
+     */
+    void removeBeforeWriteResponseListener(BeforeWriteResponseListener listener);
+
+    /**
+     * (internal api) Execute all {@link BeforeWriteResponseListener} before writing response.
+     * @param consumer the consumer to accept each listener
+     * @throws IOException if an I/O error occurs
+     */
+    void forEachBeforeWriteResponseListener(ConsumerThrowsIOException<BeforeWriteResponseListener> consumer) throws IOException;
+
+    /**
+     * (internal api) Consumer with IOException.
+     * @param <T> the type of the input to the operation
+     */
+    public interface ConsumerThrowsIOException<T> {
+        void accept(T t) throws IOException;
+    }
 }
--- integration-test/src/main/java/org/primefaces/showcase/view/data/ExCellaExporterOptionsImpl.java
+++ integration-test/src/main/java/org/primefaces/showcase/view/data/ExCellaExporterOptionsImpl.java
@@ -1,8 +1,13 @@
 package org.primefaces.showcase.view.data;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.primefaces.component.export.ExcelOptions;
 
 import net.bis5.excella.primefaces.exporter.ExCellaExporterOptions;
+import net.bis5.excella.primefaces.exporter.listener.BeforeWriteResponseListener;
 
 public class ExCellaExporterOptionsImpl extends ExcelOptions implements ExCellaExporterOptions {
 
@@ -15,6 +20,26 @@ public class ExCellaExporterOptionsImpl extends ExcelOptions implements ExCellaE
     @Override
     public boolean isThrowExceptionWhenNoData() {
         return throwExceptionWhenNoData;
+    }
+
+    private final List<BeforeWriteResponseListener> beforeWriteResponseListeners = new ArrayList<>();
+
+    @Override
+    public void addBeforeWriteResponseListener(BeforeWriteResponseListener listener) {
+        beforeWriteResponseListeners.add(listener);
+    }
+
+    @Override
+    public void removeBeforeWriteResponseListener(BeforeWriteResponseListener listener) {
+        beforeWriteResponseListeners.remove(listener);
+    }
+
+    @Override
+    public void forEachBeforeWriteResponseListener(ConsumerThrowsIOException<BeforeWriteResponseListener> consumer)
+            throws IOException {
+        for (BeforeWriteResponseListener listener : beforeWriteResponseListeners) {
+            consumer.accept(listener);
+        }
     }
 
 }
